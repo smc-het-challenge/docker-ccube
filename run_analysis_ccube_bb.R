69b0b8a54730884aa7cb2d1cbdd240c7bcaf60bc
Ke Yuan <keyuan@glaroam2-144-110.wireless.gla.ac.uk>
2019-04-23 14:26:12 +0100

    clean up

--- run_analysis_ccube_bb.R
+++ run_analysis_ccube_bb.R
@@ -13,34 +13,8 @@ registerDoParallel(cores = detectCores())
 args <- commandArgs(trailingOnly = TRUE)
 vcfFile <- as.character(args[1])
 batternbergFile <- as.character(args[2])
+purityFile <- as.character(args[3])
 
-testSample =4
-
-if (testSample == 1) {
-  vcfFile <- "~/Downloads/SMC-Het/P1-noXY/P1-noXY.mutect.vcf"
-  batternbergFile <- "~/Downloads/SMC-Het/P1-noXY/P1-noXY.battenberg.txt"
-  purityFile <- "~/Downloads/SMC-Het/P1-noXY/P1-noXY.cellularity_ploidy.txt"
-  
-}
- 
-if (testSample == 2) {
-  vcfFile <- "~/Downloads/SMC-Het/P7-noXY/P7-noXY.mutect.vcf"
-  batternbergFile <- "~/Downloads/SMC-Het/P7-noXY/P7-noXY.battenberg.txt"
-  purityFile <- "~/Downloads/SMC-Het/P7-noXY/P7-noXY.cellularity_ploidy.txt"
-} 
-
-if (testSample == 3) { 
-  vcfFile <- "~/Downloads/SMC-Het/S3-noXY/S3-noXY.mutect.vcf"
-  batternbergFile <- "~/Downloads/SMC-Het/S3-noXY/S3-noXY.battenberg.txt"
-  purityFile <- "~/Downloads/SMC-Het/S3-noXY/S3-noXY.cellularity_ploidy.txt "
-}  
-
-if (testSample == 4) {
-  vcfFile <- "~/Downloads/SMC-Het/T2-noXY//T2-noXY.mutect.vcf"
-  batternbergFile <- "~/Downloads/SMC-Het/T2-noXY/T2-noXY.battenberg.txt"
-  purityFile <- "~/Downloads/SMC-Het/T2-noXY/T2-noXY.cellularity_ploidy.txt"
-  
-}
 
 ssm_file <- "ssm_data.txt"
 
@@ -60,24 +34,7 @@ ssm <- read.delim(ssm_file, stringsAsFactors = F)
 cna <- read.delim(batternbergFile, stringsAsFactors = F)
 ssm <- ParseSnvCnaBattenberg(ssm, cna) 
 
-# Estimate purity and write 1A.txt
-if (testSample == 1) {
-  cellularity = 0.87
-}
-
-if (testSample == 2) {
-  cellularity = 0.81
-}
-
-if (testSample == 3) {
-  cellularity = 0.73
-}
-
-if (testSample == 4) {
-  cellularity = 0.56
-}
-
-#cellularity <- GetPurity(ssm)
+# Get Battenberg purity
 cellularity <-read.delim(purityFile, stringsAsFactors=FALSE)$cellularity
 ssm$purity <- cellularity
 write.table(cellularity, file = "1A.txt", sep = "\t", row.names = F, col.names = F, quote = F)
@@ -90,7 +47,7 @@ ProblemSsmIDs <- filter(allSsm, major_cn == 0 )$id
 ssm <- filter( allSsm, major_cn > 0 )
 
 
-# 2nd filter: fp_qval, rough_ccf1, rough_ccf0, total_counts, var_counts 
+# 2nd filter: fp_qval, rough_ccf1, rough_ccf0
 ssm<- mutate(rowwise(ssm), 
              fp_pval = binom.test(var_counts, total_counts, 5e-2, alternative = "great")$p.value, 
              fp_qval = p.adjust(fp_pval, method = "hochberg")
@@ -116,8 +73,8 @@ ProblemSsmIDs <- c(ProblemSsmIDs, filter(ssm, fp_qval > 0.05  &
 ssm = filter( ssm, !id %in% ProblemSsmIDs)
 
 # Run Ccube 
-numOfClusterPool = 6
-numOfRepeat = 1
+numOfClusterPool = 1:7
+numOfRepeat = 5
 ccubeRes <- RunCcubePipeline(ssm = ssm, 
                              numOfClusterPool = numOfClusterPool, numOfRepeat = numOfRepeat,
                              runAnalysis = T, runQC = T)
@@ -137,13 +94,12 @@ if (!is.matrix(ccubeRes$res$full.model$responsibility)) {
   colnames(mutR) <- paste0("cluster_", seq_along(uniqLabels) )
 }
 
-# data frame of ccube output
 ssmCcube <- ccubeRes$ssm[, "id"]
 ssmCcube <- cbind(ssmCcube, mutR)
 ssmCcube <- cbind(ssmCcube, matrix(0, nrow = nrow(ssmCcube), 1 ) )
 colnames(ssmCcube)[ncol(ssmCcube)] <- paste0("cluster_", length(uniqLabels) + 1 )
 ssmCcube$label <- apply(mutR, 1, which.max)
-# data frame of false postives
+
 ProblemSsm <- data.frame( id =  ProblemSsmIDs )
 ProblemSsmMutR <-  data.frame ( matrix(0, nrow = nrow(ProblemSsm), ncol = length(uniqLabels)  ) )
 colnames(ProblemSsmMutR) <- paste0("cluster_", seq_along(uniqLabels))
@@ -158,10 +114,9 @@ rownames(tt)<-NULL
 
 clusterCertainty <- as.data.frame(table(tt$label), stringsAsFactors = F)
 clusterCertainty <- rename(clusterCertainty, cluster = Var1, n_ssms = Freq)
-clusterCertainty$proportion <- c ( ccubeRes$res$full.model$ccfMean[sort(uniqLabels)][as.integer(clusterCertainty$cluster[-nrow(clusterCertainty)] )] * cellularity, 
+clusterCertainty$proportion <- c ( ccubeRes$res$full.model$ccfMean[as.integer(clusterCertainty$cluster[-nrow(clusterCertainty)] )] * cellularity, 
                                    0)
 write.table(clusterCertainty, file = "1C.txt", sep = "\t", row.names = F, col.names=F, quote = F)
-
 write.table(tt$label, file = "2A.txt", sep = "\t", row.names = F, col.names=F, quote = F)
 
 tt$id <- NULL
